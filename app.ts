518da57eced1ae85edc6f61431249b3d67e86236
unknown <Zaidrehan545@example.com>
2024-04-14 01:50:18 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+
+
+console.log("Guess a number between 1-10");
+const randomNumber = Math.floor(Math.random() * 9 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Please guess a number 1 to 10:",
+    }
+]);
+
+if (answer.UserGuessedNumber === randomNumber){
+    console.log("Congrats! You guessed the right number.")
+}
+else {
+    console.log("Better luck next time! You guessed a wrong number")
+}; 
+
+console.log (`The right number was: ${randomNumber}`)
